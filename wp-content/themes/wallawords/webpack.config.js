6476db05b4611a131b069c12c13000d750292d70
Glide - Pooja <aakash@glidedesign.com>
2025-09-03 12:55:00 +0530

    Design changes

--- wp-content/themes/wallawords/webpack.config.js
+++ wp-content/themes/wallawords/webpack.config.js
@@ -5,6 +5,7 @@ const path = require( 'path' );
 const FixStyleOnlyEntriesPlugin = require( 'webpack-fix-style-only-entries' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const CopyPlugin = require( 'copy-webpack-plugin' );
+const TerserPlugin = require("terser-webpack-plugin");
 const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
 const RemovePlugin = require( 'remove-files-webpack-plugin' );
 
@@ -13,6 +14,7 @@ const isProduction = process.env.NODE_ENV === 'production';
  * WordPress Dependencies
  */
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config.js' );
+
 
 module.exports = {
 	...defaultConfig,
@@ -30,6 +32,7 @@ module.exports = {
 			clean: true,
 		},
 	},
+
 	module: {
 		...defaultConfig.module,
 		rules: [
@@ -68,6 +71,14 @@ module.exports = {
 				},
 			},
 			{
+				    test: /confetti\.min\.js$/, // match exactly this file
+					type: "asset/resource",
+					generator: {
+					   filename: "game/[name][ext]", // keep in game/ folder
+					}
+
+			},
+			{
 				test: /\.svg$/,
 				issuer: /\.html$/,
 				type: 'asset/resource',
@@ -77,6 +88,14 @@ module.exports = {
 			},
 		],
 	},
+	optimization: {
+    	minimize: true,
+    	minimizer: [
+     	 new TerserPlugin({
+       		 exclude: /confetti\.min\.js/, // <- Don't minify this file
+     	 }),
+   		 ],
+	  },
 	plugins: [
 		...defaultConfig.plugins,
 		new FixStyleOnlyEntriesPlugin(), //removes extra generated files.
@@ -131,7 +150,13 @@ module.exports = {
 					to: 'images/social-icons',
 					noErrorOnMissing: true,
 				},
+				{
+					from: "assets/src/js/game/confetti.min.js",
+					to: "game/confetti.min.js",
+					noErrorOnMissing: true,
+				},
 			],
+		
 		} ),
 		new RemovePlugin( {
 			after: {
