644035dd17b6c8a48b9006654f2de28d21a0629c
Glide-Dhara <aakash@glidedesign.com>
2025-09-04 17:55:39 +0530

    Backend section container design changes

--- wp-content/themes/wallawords/src/edit.jsx
+++ wp-content/themes/wallawords/src/edit.jsx
@@ -63,6 +63,7 @@ export default function Edit( props ) {
 								options={ [
 
 									{ label: 'Width 1180px (Default)', value: 'ctn' },
+									{ label: 'Width 1080px', value: 'ctn-1080' },
 									{ label: 'Width 980px', value: 'ctn-980' },
 									{ label: 'Width 760px', value: 'ctn-760' },
 								]
