7b02a3de3924a106310bc5434765609fb4f8fa96
Glide - Pooja <aakash@glidedesign.com>
2025-09-08 19:08:30 +0530

    Design changes

--- wp-content/themes/wallawords/header.php
+++ wp-content/themes/wallawords/header.php
@@ -339,7 +339,7 @@ $page_template = basename(get_page_template());
                     </div>
                     <?php endforeach;?>
 
-                    <div class="button-container flex-button">
+                    <div class="button-container flex-column-button">
                         <a id="play-button" class="button">Start Playing</a>
                         <a id="play-button" class="button">Skip to Today's Puzzle</a>
                     </div>
