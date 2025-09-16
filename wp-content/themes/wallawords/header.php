acdf316a020941bb763b738efe478cfae8105e3e
Glide - Pooja <aakash@glidedesign.com>
2025-09-16 19:06:23 +0530

    Design changes

--- wp-content/themes/wallawords/header.php
+++ wp-content/themes/wallawords/header.php
@@ -59,17 +59,17 @@ $page_template = basename(get_page_template());
         href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/build/images/pwa/icon.svg">
     <link rel="manifest"
         href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/build/images/pwa/site.webmanifest">
-    <meta name="theme-color" content="#0047FE">
+    <meta name="theme-color" content="#1D1D1DBC">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="application-name" content="Base Theme Package">
     <!-- Windows Phone -->
-    <meta name="msapplication-navbutton_color" content="#0047FE">
-    <meta name="msapplication-TileColor" content="#0047FE">
+    <meta name="msapplication-navbutton_color" content="#1D1D1DBC">
+    <meta name="msapplication-TileColor" content="#1D1D1DBC">
     <meta name="msapplication-tap-highlight" content="no">
     <meta name="msapplication-TileImage"
         content="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/build/images/pwa/pwa-icon-144.png">
     <!-- iOS Safari -->
-    <meta name="apple-mobile-web-app-status-bar-style" content="#0047FE">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#1D1D1DBC">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.10.2/lottie.min.js"></script>
     <?php
 		// Tracking Code.
