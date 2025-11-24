f6358598de64c3b6416583880437b6d6418a925f
Glide-Ashish <aakash@glidedesign.com>
2025-11-24 16:00:35 +0530

    update SMS message for share message

--- wp-content/themes/wallawords/assets/src/js/game/wallawords-game.js
+++ wp-content/themes/wallawords/assets/src/js/game/wallawords-game.js
@@ -285,7 +285,7 @@ function initializeGame() {
 
             let message = `Check out my latest Walla score! I solved it with “${health}” health left 🕵. Think you can beat me? Try it here - https://wallawords.com/play`;
 
-            if (document.querySelector('#final-result-faild').classList.contains('final-result-faild')) {
+            if (document.querySelector('#final-score-screen').classList.contains('final-result-faild')) {
                 const difficulty = difficultyPopup
                     .querySelector('.level-tabs .active')
                     .getAttribute('data-level');
