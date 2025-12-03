dbca06daf3ecba76bbe7f67a298f7ddf230af664
Glide-Ashish <aakash@glidedesign.com>
2025-12-03 19:25:16 +0530

    bugs fixed

--- wp-content/themes/wallawords/assets/src/js/game/wallawords-game.js
+++ wp-content/themes/wallawords/assets/src/js/game/wallawords-game.js
@@ -1327,6 +1327,7 @@ function handleFailure(isAcademy) {
 
 function handleSuccess(isAcademy) {
     finalScoreScreen.classList.remove('final-result-faild');
+    document.body.classList.add('final-result');
     if (!isAcademy) {
         showRegularSuccess();
     } else {
